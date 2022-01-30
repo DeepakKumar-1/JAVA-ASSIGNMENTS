8aa5572f4580f21692e31f77e5e724d2fbd260a5
Deepak Kumar <85002425+DeepakKumar-1@users.noreply.github.com>
2022-01-30 12:55:40 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,51 +1,15 @@
-package com.company.Trees;
-
-
-import java.util.*;
+package com.company.PriorityQueue;
 
 public class Main {
-
-    public static TreeNode<Integer> takeInput(){
-        int n;
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Enter Next Node Data: ");
-        n = sc.nextInt();
-        TreeNode<Integer>  root = new TreeNode<Integer>(n);
-        System.out.print("Enter Number of Children for " + n + " ");
-        int childCount = sc.nextInt();
-        for(int i=0; i<childCount; i++){
-            TreeNode<Integer> child = takeInput();
-            root.children.add(child);
+    public static void main(String [] args) throws PriorityQueueException{
+        Priority_Queue pq = new Priority_Queue();
+        int []arr = {5,1,9,2,0};
+        for (int element : arr){
+            pq.insert(element);
         }
-        return root;
-    }
-
-    public static void print(TreeNode<Integer> root){
-        String s = root.data + ":";
-        for(int i=0; i<root.children.size(); i++){
-            s = s + root.children.get(i).data + ", ";
+        while(!pq.isEmpty()){
+            System.out.print(pq.removeMin() + " ");
         }
-        System.out.println(s);
-        for(int i=0; i< root.children.size(); i++){
-            print(root.children.get(i));
-        }
-    }
-    public static void main(String [] args){
-        TreeNode<Integer> root = takeInput();
-        print(root);
-//        TreeNode<Integer> root = new TreeNode<>(4);
-//        TreeNode<Integer> node1 = new TreeNode<>(2);
-//        TreeNode<Integer> node2 = new TreeNode<>(3);
-//        TreeNode<Integer> node3 = new TreeNode<>(5);
-//        TreeNode<Integer> node4 = new TreeNode<>(6);
-//        root.children.add(node1);
-//        root.children.add(node2);
-//        root.children.add(node3);
-//
-//        System.out.println(root);
-
-//        node2.children.add(node4);
-
-
+        System.out.println();
     }
 }
