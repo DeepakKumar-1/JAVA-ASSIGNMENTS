8a9597b8a5388151f1dc2be5a42053557281a9fb
Deepak Kumar <85002425+DeepakKumar-1@users.noreply.github.com>
2022-02-17 23:05:03 +0530

    Add files via upload

--- Greeting.java
+++ Greeting.java
@@ -1,21 +1,9 @@
-package com.company;
+package ObjectOrientedProg.packages;
 
-import java.util.Scanner;
+import static ObjectOrientedProg.packages.Message.message;
 
 public class Greeting {
-        // Take name as input and print a greeting message for that name.
-        public static void main(String[] args) {
-
-            // Creating Scanner Object for taking input from user
-            Scanner sc = new Scanner(System.in);
-
-            // Taking Name as a input From the User
-            System.out.print(" Enter Your Name: ");
-            String name = sc.nextLine();
-
-            // Displaying Greeting Message
-            System.out.println(" Good Morning " + name);
-
-        }
+    public static void main(String [] args){
+        message("Deepak");
     }
-
+}
---
+++ Message.java
@@ -0,0 +1,9 @@
+package ObjectOrientedProg.packages;
+
+public class Message {
+    public static void main(String [] args){
+    }
+    static void message(String name){
+        System.out.println(" Hello  " + name);
+    }
+}
